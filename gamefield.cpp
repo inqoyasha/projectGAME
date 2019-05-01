7b69d70981938938f43b95932fd1cad37038e4fa
Aza <shamsutdinovxd@yandex.ru>
2019-05-01 22:29:33 +0300

    v2.1

--- gamefield.cpp
+++ gamefield.cpp
@@ -7,30 +7,110 @@
 GameField::GameField(QQuickItem *parent)
     :QQuickPaintedItem(parent)
 {
-
 }
 
 void GameField::paint(QPainter *painter)
 {
-    QPainterPath path;
+    reset();
+    draw_grid(painter);
+    draw_objects(painter);
 
-    path.lineTo(width(),0);
-    path.lineTo(width(),height());
-    path.lineTo(0,height());
-    painter->setPen(Qt::NoPen);
-    painter->fillPath(path, QBrush(m_color));
 }
 
-QColor GameField::color() const
+void GameField::draw_grid(QPainter *painter)
 {
-    return m_color;
-}
-
-void GameField::setColor(QColor color)
-{
-    if (m_color != color){
-        m_color = color;
-        emit colorChanged(m_color);
-        update();
+    for (int i=0; i<=m_columns; ++i) {
+        painter->drawLine((width()/m_columns)*i, 0, (width()/m_columns)*i, height());
+    }
+    for (int i=0; i<=m_rows; ++i) {
+        painter->drawLine(0, (height()/m_rows)*i, width(), (height()/m_rows)*i);
     }
 }
+
+void GameField::draw_objects(QPainter *painter)
+{
+    for (auto i = m_field.begin(); i != m_field.end(); ++i) {
+        switch (*i) {
+            case BOX:
+                qDebug() << "я вызвалсяB";
+                painter->fillRect(width()/m_columns, 0, width()/m_columns, height(),Qt::blue);
+                break;
+            case WALL:
+                qDebug() << "я вызвалсяW";
+                painter->fillRect(width()/m_columns, 0, width()/m_columns, height(),Qt::black);
+                break;
+            case PLAYER:
+                qDebug() << "я вызвалсяP";
+                painter->fillRect(width()/m_columns, 0, width()/m_columns, height(),Qt::red);
+                break;
+            case EMPTY:
+                qDebug() << "я вызвалсяE";
+                painter->fillRect(width()/m_columns, 0, width()/m_columns, height(),Qt::white);
+                break;
+            case BOX_POSITION:
+                qDebug() << "я вызвалсяBP";
+                painter->fillRect(width()/m_columns, 0, width()/m_columns, height(),Qt::yellow);
+                break;
+        }
+    }
+}
+
+void GameField::reset()
+{
+  //  QLinkedList<eField> startField; //tmpRoom
+    for (int i=0; i<6; ++i) m_field.append(WALL);
+
+    m_field.append(WALL);
+    for (int i=0; i<4; ++i) m_field.append(EMPTY);
+    m_field.append(WALL);
+
+    m_field.append(WALL);
+    m_field.append(PLAYER);
+    m_field.append(WALL);
+    m_field.append(BOX);
+    m_field.append(EMPTY);
+    m_field.append(WALL);
+
+    m_field.append(WALL);
+    m_field.append(EMPTY);
+    m_field.append(WALL);
+    m_field.append(BOX_POSITION);
+    m_field.append(EMPTY);
+    m_field.append(WALL);
+
+    m_field.append(WALL);
+    for (int i=0; i<4; ++i) m_field.append(EMPTY);
+    m_field.append(WALL);
+
+    for (int i=0; i<6; ++i) m_field.append(WALL);
+
+    for (auto i = m_field.begin(); i != m_field.end(); ++i) {
+        qDebug() << *i;
+    }
+}
+
+void GameField::stepBack()
+{
+}
+
+bool GameField::isComplete()
+{
+    return m_isComplete;
+}
+
+qint32 GameField::stepsCount()
+{
+    return m_stepsCount;
+}
+
+qint32 GameField::boxPosition()
+{
+    return m_boxPosition;
+}
+
+qint32 GameField::boxOnPosition()
+{
+    return m_boxOnPosition;
+}
+
+
--- gamefield.h
+++ gamefield.h
@@ -4,23 +4,52 @@
 #include <QQuickItem>
 #include <QQuickPaintedItem>
 #include <QColor>
+#include <QLinkedList>
+
+#include <player.h>
 
 class GameField : public QQuickPaintedItem
 {
     Q_OBJECT
-    Q_PROPERTY(QColor color READ color WRITE setColor NOTIFY colorChanged)
 public:
     explicit GameField(QQuickItem *parent = nullptr);
-    void paint(QPainter *painter) Q_DECL_OVERRIDE;
-    QColor color() const;
-    void setColor(QColor color);
+    void paint(QPainter *painter) Q_DECL_OVERRIDE; //otrisovka?
+
+    void draw_grid(QPainter *painter);
+    void draw_objects(QPainter *painter);
+    void draw_stats(QPainter *painter);
+
+    Q_INVOKABLE void reset(); //sbros
+    Q_INVOKABLE void stepBack(); // shag nazad
+    bool isComplete();
+
+    qint32 stepsCount(); //sdelannie shagi
+    qint32 boxPosition(); //skolko mest pod gruz
+    qint32 boxOnPosition(); //skolko mest zanyato
+
+    enum eField {
+        EMPTY,
+        BOX,
+        WALL,
+        PLAYER,
+        BOX_POSITION,
+        BOX_ON_POSITION,
+        PLAYER_ON_POSITION
+    };
 
 signals:
-    void colorChanged(QColor color);
-public slots:
-private:
-    QColor m_color = QColor("grey");
 
+public slots:
+
+private:
+    QLinkedList<eField> m_field; //mRoom
+    qint32 m_columns = 6;
+    qint32 m_rows = 6;
+    qint32 m_stepsCount;
+    qint32 m_boxPosition;
+    qint32 m_boxOnPosition;
+    bool m_isComplete = false;
+    QPair<qint32,qint32> m_playerPosition;
 };
 
 #endif // GAMEFIELD_H
